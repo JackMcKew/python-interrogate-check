16b910a2bdfe2391058d7831f88b7bb97e2906fc
Fizban <jackmckew2@gmail.com>
2020-05-21 22:59:16 +1000

    Remove echo

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,6 @@ if interrogate --generate-badge $2 $1 | grep -q 'PASSED'; then
     interrogate --generate-badge $2 $1
     exit 0
 else 
-    echo interrogate --generate-badge $2 $1
+    interrogate --generate-badge $2 $1
     exit 1
 fi
