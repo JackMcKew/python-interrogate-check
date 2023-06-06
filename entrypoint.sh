ed71010aba41497e7b6a145781077a4991d6fe24
Simon <98581029+shelmigtwo@users.noreply.github.com>
2023-06-06 12:15:45 +0100

    Feat: Adds ignore-module option to python-interrogate-check GitHub action

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,10 +3,13 @@ set -eax
 
 interrogate --version
 
+
+IGNORE_MODULE=$([[ "$4" == true ]] && echo '-M' || echo '')
+
 if interrogate --fail-under $2 --generate-badge $3 $1 | grep -q 'PASSED'; then
-    interrogate --fail-under $2 --generate-badge $3 $1
+    interrogate --fail-under $2 --generate-badge $3 $IGNORE_MODULE $1
     exit 0
 else 
-    interrogate --fail-under $2 --generate-badge $3 $1
+    interrogate --fail-under $2 --generate-badge $3 $IGNORE_MODULE $1
     exit 1
 fi
