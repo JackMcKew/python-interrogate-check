87bfcbf03355128c99b5fe452436492b9280c478
cameron-currie <cameron.currie@dsit.gov.uk>
2024-11-12 14:03:38 +0000

    Feat: Add verbose option to entrypoint.sh for detailed output

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,13 +3,13 @@ set -eax
 
 interrogate --version
 
-
 IGNORE_MODULE=$([[ "$4" == true ]] && echo '-M' || echo '')
+VERBOSE=$([[ "$5" == true ]] && echo '--verbose' || echo '')
 
 if interrogate --fail-under $2 --generate-badge $3 $1 | grep -q 'PASSED'; then
     interrogate --fail-under $2 --generate-badge $3 $IGNORE_MODULE $1
     exit 0
 else 
-    interrogate --fail-under $2 --generate-badge $3 $IGNORE_MODULE $1
+    interrogate --fail-under $2 --generate-badge $3 $IGNORE_MODULE $1 $VERBOSE
     exit 1
 fi
